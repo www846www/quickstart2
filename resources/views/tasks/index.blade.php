fedbd4c43e45feaee0319d0b363476f7099ee7c1
www846www <e0981298711@gmail.com>
2017-10-17 17:03:05 +0800

    檢視任務

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,37 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Task
+                </div>
 
-    <!-- Bootstrap 樣板... -->
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                   
 
-    <div class="panel-body">
-        <!-- 顯示驗證錯誤 -->
- 
+                    <!-- New Task Form -->
+                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
 
-        <!-- 新任務的表單 -->
-        <form action="/task" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task-name" class="col-sm-3 control-label">Task</label>
 
-            <!-- 任務名稱 -->
-            <div class="form-group">
-                <label for="task-name" class="col-sm-3 control-label">任務</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                            </div>
+                        </div>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-btn fa-plus"></i>Add Task
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
 
-            <!-- 增加任務按鈕-->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> 增加任務
-                    </button>
+            <!-- Current Tasks -->
+            @if (count($tasks) > 0)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Current Tasks
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th>Task</th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($tasks as $task)
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+
+                                        <!-- Task Delete Button -->
+                                        <td>
+                                            <form action="{{url('task/' . $task->id)}}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </form>
+            @endif
+        </div>
     </div>
-
-    <!-- 代辦：目前任務 -->
- 
 @endsection
