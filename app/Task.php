a86078bbd1686a87d5331a258defc9963eb2bf32
www846www <e0981298711@gmail.com>
2017-10-17 16:07:18 +0800

    Elequent關連與資料庫遷移

---
+++ app/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * 這些屬性能被批量賦值。
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
